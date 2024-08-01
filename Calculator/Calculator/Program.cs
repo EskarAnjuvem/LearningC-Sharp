c2928c77e06e7c0e9b867aaca3937074eeeff66b
Sanjeev Kumar Mahato <eskar.anjuvem@gmail.com>
2024-08-01 17:45:16 +0530

    Some more Learning

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal firstNum, secondNum;
+            string choice;
+
+            Console.WriteLine("Calculator App");
+
+            Console.WriteLine("Enter Your First Number:");
+            firstNum = Convert.ToDecimal (Console.ReadLine());
+            Console.WriteLine("Enter Your Second Number:");
+            secondNum = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Press a for Addition, s for Subtraction, m for Multiplication and any other key for division");
+            choice = Console.ReadLine();
+
+            if (choice == "a")
+            {
+                Console.WriteLine("The Sum is " + (firstNum + secondNum));
+            }
+            else if (choice == "s")
+            {
+                Console.WriteLine("The difference is " + (firstNum - secondNum));
+            }
+            else if (choice == "m")
+            {
+                Console.WriteLine("The product is " + (firstNum * secondNum));
+            }
+            else
+            {
+                Console.WriteLine("The quotient is " + (firstNum / secondNum));
+            }
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
