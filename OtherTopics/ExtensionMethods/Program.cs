64dcce7c5be44ce699f4d61df9f602bc5ed5a875
Sanjeev Kumar Mahato <eskar.anjuvem@gmail.com>
2024-09-04 12:53:52 +0530

    Extension Methods

---
+++ OtherTopics/ExtensionMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: => {1} ",
+                obj.GetType().Name,
+                Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+
+        public static int ReverseDigits(this int number)
+        {
+            char[] digits = number.ToString().ToCharArray();
+            Array.Reverse(digits);
+            string newDigits = new string(digits);
+            return int.Parse(newDigits);
+        }
+
+
+
+    }
+
+}
+
+
---
+++ OtherTopics/ExtensionMethodsTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace ExtensionMethodsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12345;
+            number.DisplayDefiningAssembly();            
+            Console.WriteLine("Reverse of {0} is {1}", number, number.ReverseDigits());
+
+
+            Console.ReadLine();
+        }
+    }
+}
